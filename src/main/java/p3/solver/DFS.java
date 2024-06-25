0fe48b48eace7e2901f5d90d78441b4be8dd31e5
LukasKlenner <klenner-lukas@t-online.de>
2024-06-25 11:14:14 +0200

    Use explicit maps to store state of dfs algorithm according to lecture

--- src/main/java/p3/solver/DFS.java
+++ src/main/java/p3/solver/DFS.java
@@ -1,10 +1,9 @@
 package p3.solver;
 
-import p3.graph.Edge;
 import p3.graph.Graph;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.ObjIntConsumer;
 
 /**
@@ -26,13 +25,27 @@ public class DFS<N> implements GraphTraverser<N> {
     protected final Graph<N> graph;
 
     /**
-     * A set of all nodes that have already been visited. A node being added this set is equivalent to it being
-     * colored gray.
+     * Stores the current color of each node in the graph.
      */
-    protected final Set<N> visited;
+    protected final Map<N, Color> colors;
 
     /**
-     * Stores the current timestep during the visiting by the DFS algorithm.
+     * Stores the discovery time of each node in the graph. It only contains values for nodes that have been discovered.
+     */
+    protected final Map<N, Integer> discoveryTimes;
+
+    /**
+     * Stores the finish time of each node in the graph. It only contains values for nodes that have been finished.
+     */
+    protected final Map<N, Integer> finishTimes;
+
+    /**
+     * Stores the used predecessor of each node in the graph. It only contains values for nodes that have been discovered.
+     */
+    protected final Map<N, N> predecessors = new HashMap<>();
+
+    /**
+     * Stores the current time during the visiting by the DFS algorithm.
      */
     protected int time = 0;
 
@@ -43,48 +56,88 @@ public class DFS<N> implements GraphTraverser<N> {
      */
     public DFS(Graph<N> graph) {
         this.graph = graph;
-        this.visited = new HashSet<>();
+        this.colors = new HashMap<>();
+        this.discoveryTimes = new HashMap<>();
+        this.finishTimes = new HashMap<>();
     }
 
     @Override
     public void traverse(ObjIntConsumer<N> consumer) {
         init();
         for (N node : graph.getNodes()) {
-            if (!visited.contains(node)) {
+            if (colors.get(node) == Color.WHITE) {
                 visit(consumer, node);
             }
         }
     }
 
     /**
-     * Initializes the DFS algorithm to its starting state.
+     * Initializes the DFS algorithm to its starting state, i.e., the maps are cleared, all nodes are colored white and
+     * the time is set to 0.
      */
     protected void init() {
-        visited.clear();
+        colors.clear();
+        discoveryTimes.clear();
+        finishTimes.clear();
+        predecessors.clear();
+
+        for (N node : graph.getNodes()) {
+            colors.put(node, Color.WHITE);
+            predecessors.put(node, null);
+        }
+
         time = 0;
     }
 
     /**
      * Visits a new node in the graph.
      * <p>
-     * A node is visited by first coloring it gray and then recursively visiting all its neighbors. After all neighbors
-     * have been visited, the node is colored black, i.e., it is passed to the consumer.
+     * A node is visited by first discovering it and coloring it gray and, afterward, recursively visiting all its neighbors.
+     * After all neighbors have been visited, the node is finished, i.e., colored black and passed to the consumer.
      *
      * @param consumer Function that accepts the node and its finish time.
      * @param current  Node that is processed by this method
      */
     protected void visit(ObjIntConsumer<N> consumer, N current) {
         time++;
-        visited.add(current);
 
-        for (Edge<N> edge : graph.getOutgoingEdges(current)) {
-            if (!visited.contains(edge.to())) {
-                visit(consumer, edge.to());
+        discoveryTimes.put(current, time);
+        colors.put(current, Color.GRAY);
+
+        for (N neighbor : graph.getAdjacentNodes(current)) {
+            if (colors.get(neighbor) == Color.WHITE) {
+                predecessors.put(neighbor, current);
+                visit(consumer, neighbor);
             }
         }
 
         time++;
 
+        colors.put(current, Color.BLACK);
+        finishTimes.put(current, time);
+
         consumer.accept(current, time);
+    }
+
+    /**
+     * The colors used to mark the state of a node during the DFS algorithm.
+     */
+    protected enum Color {
+
+        /**
+         * The node has not been discovered yet. This is the initial state of all nodes.
+         */
+        WHITE,
+
+        /**
+         * The node has been discovered but not yet finished. This means that the node, or one of its neighbors
+         * recursively, is currently being processed.
+         */
+        GRAY,
+
+        /**
+         * The node has been discovered and finished. This means that the node and its neighbors have been fully processed.
+         */
+        BLACK
     }
 }
